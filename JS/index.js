20eaf1c14e88d8138bbbe0d0ca39019de34eedf0
Min-Syuan <annie0910610569gmail.com>
2023-12-06 19:42:55 +0800

    更

--- JS/index.js
+++ JS/index.js
@@ -1,27 +1,21 @@
-// const header = document.getElementById("scroll");
-// const scrollDistance = 500;
-
-// window.addEventListener("scroll", () => {
-//     if (window.scrollY > scrollDistance) {
-//         header.classList.add("scroll-menu");
-//     } else {
-//         header.classList.remove("scroll-menu");
-//     }
-//     }); 
-
-
 const newHeader = document.getElementById('scroll');
-const scrollTriggerPosition = 200; // 設置滾動觸發新 header 的位置
+const scrollTriggerPosition = 150; // 設置滾動觸發新 header 的位置
 
-// 監聽網頁滾動事件
-window.addEventListener('scroll', () => {
-  // 取得目前的垂直滾動位置
-  const scrollPosition = window.scrollY || window.pageYOffset;
+const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    newHeader.classList.toggle('scroll-menu', scrollPosition >= scrollTriggerPosition);
+};
 
-  // 如果滾動位置大於或等於觸發位置，顯示新 header，否則顯示原始 header
-  if (scrollPosition >= scrollTriggerPosition) {
-    newHeader.classList.add('scroll-menu');
-  } else {
-    newHeader.classList.remove('scroll-menu');
-  }
-});
+const handleMediaQueryChange = (mq) => {
+    if (mq.matches) {
+        window.removeEventListener('scroll', handleScroll);
+    } else {
+        window.addEventListener('scroll', handleScroll);
+    }
+};
+
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+mediaQuery.addEventListener('change', (e) => handleMediaQueryChange(e.currentTarget));
+
+handleMediaQueryChange(mediaQuery); // 將此行移至 change 監聽器外面
