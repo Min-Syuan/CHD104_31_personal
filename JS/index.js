80451a7a2cdac095fb3601e0a4f45a0569c17182
Min-Syuan <annie0910610569gmail.com>
2023-12-12 18:19:36 +0800

    更

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,21 @@
 const newHeader = document.getElementById('scroll');
-const scrollTriggerPosition = 150; // 設置滾動觸發新 header 的位置
+const scrollTriggerPosition = 150;
 
 const handleScroll = () => {
     const scrollPosition = window.scrollY;
     if (scrollPosition >= scrollTriggerPosition) {
-        newHeader.style.display = 'block'; // 显示 newHeader
+        newHeader.style.display = 'block';
         newHeader.style.display = 'flex'
         newHeader.classList.add('scroll-menu');
     } else {
-        newHeader.style.display = 'none'; // 隐藏 newHeader
+        newHeader.style.display = 'none';
         newHeader.classList.remove('scroll-menu');
     }
 };
 
 const handleMediaQueryChange = (mq) => {
     if (mq.matches) {
-        newHeader.style.display = 'none'; // 当满足媒体查询时隐藏 newHeader
+        newHeader.style.display = 'none';
         window.removeEventListener('scroll', handleScroll);
     } else {
         window.addEventListener('scroll', handleScroll);
