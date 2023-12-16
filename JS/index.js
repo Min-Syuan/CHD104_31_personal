d450f371188173d364d3862850a327594f07902d
Min-Syuan <annie0910610569gmail.com>
2023-12-16 15:36:40 +0800

    add

--- JS/index.js
+++ JS/index.js
@@ -28,3 +28,57 @@ mediaQuery.addEventListener('change', (e) => handleMediaQueryChange(e.currentTar
 
 handleMediaQueryChange(mediaQuery);
 
+
+// function $id(id) {
+//     return document.getElementById(id);
+// }
+
+// window.addEventListener('load', function(){
+//     let hotSaleItems = this.document.querySelector('.hot-sale-items')
+//     let curIndex = 0;
+//     function
+// })
+
+$(function () {
+    let divWidth = $('#sliderBoard').width()
+    let imgCount = $('#content li').length
+
+    for (let i=0; i<imgCount; i++){
+        $('#contentButton').append(`<li></li>`)
+    }
+    $('#contentButton li:first').addClass('clicked')
+
+    $('#content li').width(divWidth) //li寬
+    $('#content').width(divWidth*imgCount) //ul寬
+
+    let index = 0
+    let timer = setInterval(moveToNext, 3000)
+    $('#contentButton li').click(function(){
+        clearInterval(timer)
+
+        index = $(this).index()
+        // alert(index)
+        $('#content').animate({
+            left: divWidth * index * -1,
+        })
+
+        $(this).addClass('clicked')
+        $('#contentButton li').not(this).removeClass('clicked')
+
+        timer = setInterval(moveToNext, 3000)
+    })
+
+    function moveToNext() {
+        if(index< imgCount-1) {
+            index++;
+        }else {
+            index=0;
+        }
+
+        $('#content').animate({
+            left: divWidth * index * -1
+        })
+        $(`#contentButton li:eq(${index})`).addClass('clicked')
+        $('#contentButton li').not(`:eq(${index})`).removeClass('clicked')
+    }
+});
